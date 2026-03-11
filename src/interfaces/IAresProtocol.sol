2bf6837756da29d2fdfa1eb41877a1853ec94519
markdavid000 <davidojukwu59@gmail.com>
2026-03-11 09:57:16 +0100

    feat: adding features for transfer and upgrade proposal

--- src/interfaces/IAresProtocol.sol
+++ src/interfaces/IAresProtocol.sol
@@ -1,10 +1,12 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
+import "./IProposal.sol";
 
 interface IAresProtocol {
     function executeProposal(
         address _target,
         uint256 _value,
-        bytes calldata _data
+        bytes calldata _data,
+        IProposal.ProposalType _proposalType
     ) external returns (bool);
 }
