735df0dce04d77fe497579d240ab16ecfb1422db
markdavid000 <davidojukwu59@gmail.com>
2026-03-11 02:07:58 +0100

    feat: implemented distributor module

---
+++ src/modules/Distributor.sol
@@ -0,0 +1,72 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.23;
+
+import "../interfaces/IDistributor.sol";
+import "../interfaces/IERC20.sol";
+
+abstract contract MerkleDistributor is IDistributor {
+
+    IERC20 private _token;
+    bytes32 private _merkleRoot;
+    address private _governanceAddress;
+    mapping(address => bool) private _claimed;
+
+    constructor(address _tokenAddress, bytes32 _initialRoot, address _governance) {
+        _token = IERC20(_tokenAddress);
+        _merkleRoot = _initialRoot;
+        _governanceAddress = _governance;
+    }
+
+    function claimReward(
+        address _recipient,
+        uint256 _amount,
+        bytes32[] calldata _proof
+    ) external {
+        require(!_claimed[_recipient], "already claimed");
+
+        bytes32 leaf = keccak256(abi.encodePacked(_recipient, _amount));
+        require(_verify(_proof, _merkleRoot, leaf), "invalid proof");
+
+        _claimed[_recipient] = true;
+        (bool success) = _token.transfer(_recipient, _amount);
+
+        require(success, "failed to claim");
+
+        emit RewardClaimed(_recipient, _amount);
+    }
+
+    function updateRoot(bytes32 _newRoot) external {
+        require(msg.sender == _governanceAddress, "only governance");
+        bytes32 oldRoot = _merkleRoot;
+        _merkleRoot = _newRoot;
+        emit RootUpdated(oldRoot, _newRoot);
+    }
+
+    function hasClaimed(address _recipient) external view returns (bool) {
+        return _claimed[_recipient];
+    }
+
+    function getMerkleRoot() external view returns (bytes32) {
+        return _merkleRoot;
+    }
+
+    function _verify(
+        bytes32[] calldata _proof,
+        bytes32 _root,
+        bytes32 _leaf
+    ) internal pure returns (bool) {
+        bytes32 computedHash_ = _leaf;
+
+        for (uint256 i = 0; i < _proof.length; i++) {
+            bytes32 proofElement = _proof[i];
+
+            if (computedHash_ <= proofElement) {
+                computedHash_ = keccak256(abi.encodePacked(computedHash_, proofElement));
+            } else {
+                computedHash_ = keccak256(abi.encodePacked(proofElement, computedHash_));
+            }
+        }
+
+        return computedHash_ == _root;
+    }
+}
